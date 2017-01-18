f909a5fe926321d9f4fdfabf62d9ffc4f2adbb0c
psurya <surya@zemosolabs.com>
2017-01-18 13:36:54 +0530

    k

--- src/main/java/DeleteByQuery.java
+++ src/main/java/DeleteByQuery.java
@@ -7,11 +7,12 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
+import org.json.simple.JSONArray;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
 import java.net.InetAddress;
-
+import java.util.ArrayList;
 
 /**
  * Created by stp on 1/18/2017.
@@ -19,12 +20,13 @@ import java.net.InetAddress;
 public class DeleteByQuery {
    public static void main(String[] args) throws IOException, ParseException {
 
-      String indexName = "sampleindex";
-      String docType = "s";
+      ArrayList indexNames = new ArrayList();
+      indexNames.add("jigsaw");
+      indexNames.add(".kibana");
       String searchTerm = "nike";
 
       Settings settings = Settings.settingsBuilder()
-            .put("cluster.name", "elasticsearch").build();
+            .put("cluster.name", "presales").build();
 
 
       Client client = TransportClient.builder().settings(settings).build()
@@ -33,27 +35,33 @@ public class DeleteByQuery {
       SearchRequestBuilder srb1 = client
             .prepareSearch().setQuery(QueryBuilders.queryStringQuery(searchTerm)).setSize(1000);
 
+      Boolean resultsExist = true;
 
-      MultiSearchResponse sr = client.prepareMultiSearch()
-            .add(srb1)
-            .execute().actionGet();
-      BulkRequestBuilder bulkRequest = client.prepareBulk();
+      while(resultsExist) {
+         MultiSearchResponse sr = client.prepareMultiSearch()
+               .add(srb1)
+               .execute().actionGet();
+         BulkRequestBuilder bulkRequest = client.prepareBulk();
 
-      // You will get all individual responses from MultiSearchResponse#getResponses()
-      for (MultiSearchResponse.Item item : sr.getResponses()) {
-         for(SearchHit hit : item.getResponse().getHits()){
-            if(hit.getIndex().equalsIgnoreCase(indexName) && hit.getType().equalsIgnoreCase(docType)) {
-               bulkRequest.add(client.prepareDelete(indexName, docType, hit.getId()));
+         // You will get all individual responses from MultiSearchResponse#getResponses()
+         for (MultiSearchResponse.Item item : sr.getResponses()) {
+            for(SearchHit hit : item.getResponse().getHits()){
+               if(indexNames.indexOf(hit.getIndex()) < 0) {
+                  bulkRequest.add(client.prepareDelete(hit.getIndex(), hit.getType(), hit.getId()));
+               }
             }
          }
-      }
 
-      if(bulkRequest.numberOfActions() > 0) {
-         BulkResponse bulkResponse = bulkRequest.get();
-         if (bulkResponse.hasFailures()) {
-            System.out.println(bulkResponse.buildFailureMessage());
+         if(bulkRequest.numberOfActions() > 0) {
+            BulkResponse bulkResponse = bulkRequest.get();
+            if (bulkResponse.hasFailures()) {
+               System.out.println(bulkResponse.buildFailureMessage());
+            } else {
+               System.out.println("Successfully Deleted " + bulkRequest.numberOfActions() + " Entries");
+            }
          } else {
-            System.out.println("Successfully Deleted " + bulkRequest.numberOfActions() + " Entries");
+            resultsExist = false;
+            System.out.println("No more entities exists");
          }
       }
    }
